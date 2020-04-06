bb4b383cfd8aec4c5e6588cad259694bdc70957b
sammcfar21 <sammcfar21@gmail.com>
2020-04-06 22:06:39 +0000

    Added data collector .py file

---
+++ collector.py
@@ -0,0 +1,17 @@
+# Use this section to collect data
+
+#Get what type of data we are working with.
+print("What type of data are you entering (Expence, Plant): ")
+type = input()
+
+# Types of data - (Expence, Plant) for each type a function
+
+# Type 1 Plant
+if type == 'Plant':
+    print ('Plant')
+
+elif type == 'Expence':
+    print ('Expence')
+
+else:
+    print('Please Run this again')
